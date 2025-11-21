4a2aa67dcc130095c575284fef40f81e98090ad1
minhajakhan <minhajiialamkhan@gmail.com>
2025-11-21 14:06:50 -0500

    added front end production build dist folder to backend and enabled use with express.static

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(cors())
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 // app.use(morgan('tiny'))
+app.use(express.static('dist'))
 
 let phonebookEntries = [
     { 
